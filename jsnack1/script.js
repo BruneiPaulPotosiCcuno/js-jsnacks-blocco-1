eecb1409296f0ab56eda17f3037538548bc38d0f
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-11 16:48:55 +0100

    add-first-for

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+/*- Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).*/
+//Per 5 volte chiede alutente 5 numeri
+    //Creo un array vuoto
+    let numbers = [];
+    //Creo un ciclo
+    for (let i = 0; i < 5; i++) {
+        const userNumbers = prompt('Inserisce 5 numeri:');
+        console.log(userNumbers);
+    }
+
+

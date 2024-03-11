c0e3e5a00a78b0821e8c86b6a25dfab01838ffd9
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-11 18:04:21 +0100

    add-dispari-array

---
+++ jsnack3/script.js
@@ -0,0 +1,16 @@
+// - Crea un array vuoto.
+//   Chiedi per 6 volte all’utente di inserire un numero,
+//   se è dispari inseriscilo nell’array.
+
+// - Crea un array vuoto.
+let numbers = [];
+
+//   Chiedi per 6 volte all’utente di inserire un numero,
+for (let i = 0; i < 6; i++) {
+    const userNumber = parseInt(prompt('Inserisce 6 numeri'));
+    //Controllo se il numero e dispari
+    if (userNumber % 2 !==0) {
+    //Se vedo che il numero e dispari lo metto nel array
+    numbers.push(userNumber);
+    }
+}
